ae7d4ebdd76d509695873fddc73428e785035fef
Thammanit Chai <thammanit255014@gmail.com>
2026-03-13 18:46:50 +0700

    rewrite Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,46 +6,46 @@ import { useState } from "react"
 
 export default function Card(
 {
-    venueName,
-    imgSrc,
-    onRating
+  venueName,
+  imgSrc,
+  onRating
 }:
 {
-    venueName:string,
-    imgSrc:string,
-    onRating:(rating:number)=>void
+  venueName:string,
+  imgSrc:string,
+  onRating:(rating:number)=>void
 }){
 
-    const [rating,setRating] = useState<number|null>(0)
+  const [rating,setRating] = useState<number|null>(0)
 
-    return(
+  return(
 
-        <div className="w-[250px] shadow-lg rounded-lg bg-white p-2">
+    <div className="w-[250px] shadow-lg rounded-lg bg-white p-2">
 
-            <Image
-                src={imgSrc}
-                alt={venueName}
-                width={250}
-                height={150}
-                className="rounded-lg"
-            />
+      <Image
+        src={imgSrc}
+        alt={venueName}
+        width={250}
+        height={150}
+        className="rounded-lg"
+      />
 
-            <div className="font-semibold mt-2">
-                {venueName}
-            </div>
+      <div className="font-semibold mt-2">
+        {venueName}
+      </div>
 
-            <Rating
-                id={venueName + " Rating"}
-                name={venueName + " Rating"}
-                data-testid={venueName + " Rating"}
-                value={rating}
-                onChange={(event,newValue)=>{
-                    setRating(newValue)
-                    onRating(newValue ?? 0)
-                }}
-            />
+      <Rating
+        id={venueName + " Rating"}
+        name={venueName + " Rating"}
+        data-testid={venueName + " Rating"}
+        value={rating}
+        onChange={(event,newValue)=>{
+          setRating(newValue)
+          onRating(newValue ?? 0)
+        }}
+      />
 
-        </div>
+    </div>
 
-    )
+  )
 }
